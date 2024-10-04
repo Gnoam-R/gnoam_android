54178f40a043a86c6d2fa8abd5236de4f0002bb0
Hyung Woo Roh <67363759+rohhyungwoo@users.noreply.github.com>
2024-10-04 17:25:19 +0900

    feat: CoroutineScope 학습
    coroutinescope, runblocking

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.gnoam.coroutine"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.gnoam.coroutine"
         minSdk = 24
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
@@ -37,6 +37,7 @@ android {
 
 dependencies {
 
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.3.1")
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.appcompat:appcompat:1.7.0")
     implementation("com.google.android.material:material:1.12.0")
--- app/src/main/java/com/gnoam/coroutine/MainActivity.kt
+++ app/src/main/java/com/gnoam/coroutine/MainActivity.kt
@@ -2,10 +2,27 @@ package com.gnoam.coroutine
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import kotlinx.coroutines.*
 
 class MainActivity : AppCompatActivity() {
+    private val TAG = "MainActivity1004"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        main()
+    }
+
+    fun main() = runBlocking {
+        doWorld()
+        Log.e(TAG, "finish")
+    }
+
+    suspend fun doWorld() = coroutineScope {
+        launch {
+            delay(1000L)
+            Log.e(TAG, "world")
+        }
+        Log.e(TAG, "hello")
     }
 }
