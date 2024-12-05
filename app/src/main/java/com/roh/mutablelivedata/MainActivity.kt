bec8365aa8a8f88b06e2352712b273a47854ed9e
Hyung Woo Roh <romin1027@gmail.com>
2024-12-05 17:38:31 +0900

    feat: MutalbeLiveData 생성

---
+++ app/src/main/java/com/roh/mutablelivedata/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.roh.mutablelivedata
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.lifecycle.LifecycleObserver
+import androidx.lifecycle.LifecycleOwner
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Observer
+import com.roh.mutablelivedata.databinding.ActivityMainBinding
+
+
+class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
+
+    private var bt1LiveData = MutableLiveData<String>()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        bt1LiveData.observe(this, Observer<String> {
+            binding.tv1.text = it
+        })
+
+        binding.bt1.setOnClickListener {
+            bt1LiveData.postValue("onClicked button1")
+        }
+
+        binding.bt2.setOnClickListener {
+            bt1LiveData.postValue("onClicked button2")
+        }
+
+        binding.bt3.setOnClickListener {
+            bt1LiveData.postValue("onClicked button3")
+        }
+    }
+}
